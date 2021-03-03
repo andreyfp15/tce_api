4d50de07fae819d56ceef553b049131a3cd38f43
Andrey Felipe Pereira <andreyfp15@gmail.com>
2021-03-02 23:05:27 -0300

    second...

--- TCE/TCE_API/Controllers/WeatherForecastController.cs
+++ TCE/TCE_API/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace TCE_API.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+
         }
 
         [HttpGet]
