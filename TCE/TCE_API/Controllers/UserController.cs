f6855d7cb1f869e7be703f836d06016f170941cd
Andrey Felipe Pereira <andreyfp15@gmail.com>
2021-03-04 23:55:34 -0300

    Remove comments UserController

--- TCE/TCE_API/Controllers/UserController.cs
+++ TCE/TCE_API/Controllers/UserController.cs
@@ -9,35 +9,30 @@ namespace TCE_API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        // GET: api/<UserController>
         [HttpGet]
         public IEnumerable<UserModel> Get([FromServices]UserRepository userRepository)
         {
             return userRepository.Get();
         }
 
-        // GET api/<UserController>/5
         [HttpGet("{id}")]
         public UserModel Get([FromServices] UserRepository userRepository, int id)
         {
             return userRepository.Get(id);
         }
 
-        // POST api/<UserController>
         [HttpPost]
         public void Post([FromServices] UserRepository userRepository, [FromBody] UserModel user)
         {
             userRepository.Insert(user);
         }
 
-        // PUT api/<UserController>/5
         [HttpPut]
         public void Put([FromServices] UserRepository userRepository, [FromBody] UserModel user)
         {
             userRepository.Update(user);
         }
 
-        // DELETE api/<UserController>/5
         [HttpDelete("{id}")]
         public void Delete([FromServices] UserRepository userRepository, int id)
         {
